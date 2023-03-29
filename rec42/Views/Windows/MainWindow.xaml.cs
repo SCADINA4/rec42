c4b3814a8be20cc54042833d98b5848d499d64b8
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-03-29 13:21:35 +0300

    Добавьте файлы проекта.

---
+++ rec42/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using rec42.ViewsModels;
+
+
+namespace rec42.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainWindowViewModel).Pow((DataContext as MainWindowViewModel).Power, int.Parse(powTextBox.Text));
+            
+        
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9.]+");
+        }
+    }
+}
---
+++ rec42/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using rec42.ViewsModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace rec42.ViewsModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private double power;
+        private double step;
+
+        public double Power
+        {
+            get { return power; }
+            set { Set(ref power, value); } 
+        }
+        public double Step
+        {
+            get { return step; }    
+            set { Set(ref step, value); }   
+        }
+        public double Pow(double a, int n)
+        {
+            if (n == 1)
+                return a;
+            return Step = a * Pow(a, n - 1); 
+
+        }
+    }
+}
